5fcd7d5e4378b09c0b94de789bf3c9a323ebb2f0
error-Tawhid <snburgs4@gmail.com>
2025-09-24 01:00:20 +0600

    Annotating

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-type Persion = {
-  name: string;
-};
+// Annotating
 
-const persion: Persion = {
-  name: "Enamul",
-};
+// Type annotation is used to tell the compiler what type of value a variable holds.
+
+let Name: string = "TypeScript";
+let Numbers: number = 5;
+let isPublished: boolean = true;
+
+console.log(Name);
+console.log(Numbers);
+console.log(isPublished);
