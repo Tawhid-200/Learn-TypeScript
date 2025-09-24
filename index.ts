c0a7c7884a862238cc50b4ef56f5b8bf64f39b76
error-Tawhid <snburgs4@gmail.com>
2025-09-24 23:23:51 +0600

    Interference

--- index.ts
+++ index.ts
@@ -1,11 +1,5 @@
-// Annotating
+// Interference
 
-// Type annotation is used to tell the compiler what type of value a variable holds.
+const tech = "TypeScript";
 
-let Name: string = "TypeScript";
-let Numbers: number = 5;
-let isPublished: boolean = true;
-
-console.log(Name);
-console.log(Numbers);
-console.log(isPublished);
+console.log(typeof tech);
